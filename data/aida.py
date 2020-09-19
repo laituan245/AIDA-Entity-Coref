87f89d089d68edb96f37f92bce1f312e7e818eb0
Tuan Lai <tlai@adobe.com>
2020-09-19 13:19:40 -0500

    Fixes to AIDA tokenization (issues with ##)

--- data/aida.py
+++ data/aida.py
@@ -10,21 +10,24 @@ class AIDADocument:
         self.words = words
         self.entity_mentions = entity_mentions
 
-        doc_tokens = tokenizer.tokenize(' '.join(self.words))
+        # Build doc_tokens, self.word_starts_indexes, self.word_ends_indexes
+        doc_tokens, word_starts_indexes, word_ends_indexes, start_index = [], [], [], 0
+        for w in self.words:
+            word_tokens = tokenizer.tokenize(w)
+            doc_tokens += word_tokens
+            word_starts_indexes.append(start_index)
+            word_ends_indexes.append(start_index + len(word_tokens)-1)
+            start_index += len(word_tokens)
+        self.word_starts_indexes = word_starts_indexes
+        self.word_ends_indexes = word_ends_indexes
+        assert(len(self.word_starts_indexes) == len(self.words))
+        assert(len(self.word_ends_indexes) == len(self.words))
+
+        # Build token_windows, mask_windows, and input_masks
         doc_token_ids = tokenizer.convert_tokens_to_ids(doc_tokens)
         self.token_windows, self.mask_windows = \
             convert_to_sliding_window(doc_token_ids, 512, tokenizer)
         self.input_masks = extract_input_masks_from_mask_windows(self.mask_windows)
-
-        # Compute the starting index of each word
-        self.word_starts_indexes, self.word_ends_indexes = [], []
-        for index, word in enumerate(doc_tokens):
-            if not word.startswith('##'):
-                self.word_starts_indexes.append(index)
-                if index > 0: self.word_ends_indexes.append(index-1)
-        self.word_ends_indexes.append(len(doc_tokens)-1)
-        assert(len(self.word_starts_indexes) == len(self.words))
-        assert(len(self.word_ends_indexes) == len(self.words))
 
         # Compute gold_starts, gold_ends
         gold_starts, gold_ends = [], []
