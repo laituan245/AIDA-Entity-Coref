c764836fe5ea5f2492b9635eeaff1c3b494f9869
Tuan Lai <boy19ykienandon@yahoo.com.vn>
2022-02-21 02:37:56 -0600

    Update build_push_dockers.sh

--- build_push_dockers.sh
+++ build_push_dockers.sh
@@ -1,3 +1,3 @@
 cp /shared/nas/data/m1/tuanml2/aida_entity_coref/pretrained/model.pt model.pt
-docker build --tag laituan245/spanbert_entity_coref .
-docker push laituan245/spanbert_entity_coref
+docker build --tag laituan245/spanbert_entity_coref:aida2022 .
+docker push laituan245/spanbert_entity_coref:aida2022
